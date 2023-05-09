06d6ad0b2e5c3fc118c110e368130f20a7f85bd9
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-05-09 12:54:27 +0700

    refactor(env): add sort type
    fix sort by selector

--- app/react/portainer/environments/queries/useEnvironmentList.ts
+++ app/react/portainer/environments/queries/useEnvironmentList.ts
@@ -12,10 +12,13 @@ import { queryKeys } from './query-keys';
 
 export const ENVIRONMENTS_POLLING_INTERVAL = 30000; // in ms
 
+export const SortOptions = ['Name', 'Group', 'Status'] as const;
+
+export type SortType = typeof SortOptions[number];
 export interface Query extends EnvironmentsQueryParams {
   page?: number;
   pageLimit?: number;
-  sort?: string;
+  sort?: SortType;
   order?: 'asc' | 'desc';
 }
 
