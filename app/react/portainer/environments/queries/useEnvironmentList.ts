678e146df0fdf983ecabd2d6e4954c3bd3872f69
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-06-04 14:31:01 +0700

    fix(environments): fix format

--- app/react/portainer/environments/queries/useEnvironmentList.ts
+++ app/react/portainer/environments/queries/useEnvironmentList.ts
@@ -14,7 +14,7 @@ export const ENVIRONMENTS_POLLING_INTERVAL = 30000; // in ms
 
 export const SortOptions = ['Name', 'Group', 'Status'] as const;
 
-export type SortType = typeof SortOptions[number];
+export type SortType = (typeof SortOptions)[number];
 export interface Query extends EnvironmentsQueryParams {
   page?: number;
   pageLimit?: number;
